eaac3ec479485901cf819bb7f138593b243bb975
jungwon <mjw29043@gmail.com>
2022-07-18 21:50:30 +0900

    test

--- src/main/java/DBPKG/DBConnect.java
+++ src/main/java/DBPKG/DBConnect.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBConnect {
 	public static Connection getConnection()throws Exception{
 		Class.forName("oracle.jdbc.OracleDriver");
-<<<<<<< HEAD
+
 		Connection con = 
 				DriverManager.getConnection(
 			"jdbc:oracle:thin:@//192.168.7.14:1521/xe",
@@ -21,12 +21,3 @@ public class DBConnect {
 
 
 
-
-=======
-		Connection con = DriverManager.getConnection(
-				"jdbc:oracle:thin:@//192.168.7.14:1521/xe",
-											"team3","1234");
-		return con;
-}
-}
->>>>>>> b67334da950f1525542e6289073b76d40ccf1b6f
